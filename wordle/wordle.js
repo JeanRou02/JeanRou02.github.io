6cf5c7a7d9242d72b957ed595cb8036d2b240fd4
JeanRou <jeannicolas.rouette@ucalgary.ca>
2023-02-16 17:32:12 -0700

    Viewport Changes

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -1,4 +1,14 @@
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    let height = window.innerHeight;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty('--vw', `${height}px`);
+  });
+
+
 let number = Number.parseInt(Math.random() * 20);
 
 document.addEventListener("DOMContentLoaded", () => {
